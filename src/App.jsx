59149ec4dd89ef3380e7cb2c19f453957fa235fa
PatrickBDJ <dyva@live.dk>
2023-04-07 13:24:05 +0200

    Start

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Frontpage from './components/frontpage';
+
+// CSS
+import './components/css/header.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Frontpage />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/frontpage.jsx
@@ -0,0 +1,15 @@
+
+
+function Frontpage(){
+    return(
+    <div className="content">
+        <div className="header">
+            <h1>Hello</h1>
+        </div>
+    </div>
+    )
+}
+
+
+
+export default Frontpage;
