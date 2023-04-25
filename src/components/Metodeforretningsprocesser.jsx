6f965615206607bcde146b6e26d086bfdaea6bea
PatrickBDJ <dyva@live.dk>
2023-04-25 13:09:24 +0200

    Layout v.1

--- src/components/Metodeforretningsprocesser.jsx
+++ src/components/Metodeforretningsprocesser.jsx
@@ -1,7 +1,34 @@
 export default function Metodeforretningsprocesser(){
     return (
     <div className="page-top-margin">
-        <h1>Metode for forretningsprocesser</h1>
+        <div className="page-container">
+            <h1>Metode for forretningsprocesser</h1>
+            <div className="model-page-row">
+                    <div className="model-page-column">
+                        {/* <h3>Generelt om modeller</h3> */}
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, omnis hic numquam repellat minus sapiente. Nisi est debitis laboriosam libero nobis, iure animi quod ullam quam eos, nulla sunt incidunt ratione possimus, obcaecati eius exercitationem perspiciatis vitae commodi fugit consequatur cupiditate? Voluptates voluptate ut laboriosam beatae nobis iusto est dolor?</p>
+                    </div>
+                    <div className="model-page-column">
+                        <img src={require ('../assets/goats.jpg')} alt="geder" id="test-goats"/>
+                    </div>
+                </div>
+                <div className="model-page-row">
+                    <div className="model-page-column">
+                        <h3>Vejledning til QLM</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, omnis hic numquam repellat minus sapiente. Nisi est debitis laboriosam libero nobis, iure animi quod ullam quam eos, nulla sunt incidunt ratione possimus, obcaecati eius exercitationem perspiciatis vitae commodi fugit consequatur cupiditate? Voluptates voluptate ut laboriosam beatae nobis iusto est dolor?</p>
+                    </div>
+                    <div className="model-page-column">
+                        <h3>Koncern Forandringsunivers</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur maxime nesciunt debitis aperiam, corporis assumenda dolor laborum. At quos hic id doloremque recusandae delectus, dolor reprehenderit dolorem reiciendis vitae iste repellat earum a sint consectetur officia! Veniam ullam velit cum natus reprehenderit aliquam. Maxime vitae distinctio sequi nisi iure saepe?</p>
+                    </div>
+                </div>
+                <div className="model-page-row">
+                    <div className="model-page-column">
+                        <h3>IT Operation Model (ITOM)</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, omnis hic numquam repellat minus sapiente. Nisi est debitis laboriosam libero nobis, iure animi quod ullam quam eos, nulla sunt incidunt ratione possimus, obcaecati eius exercitationem perspiciatis vitae commodi fugit consequatur cupiditate? Voluptates voluptate ut laboriosam beatae nobis iusto est dolor?</p>
+                    </div>
+                </div>
+            </div>
     </div>
     )
 }
--- src/components/Modeller.jsx
+++ src/components/Modeller.jsx
@@ -5,12 +5,28 @@ export default function Modeller(){
             <h1>Modeller</h1>
             <div className="model-page-row">
                 <div className="model-page-column">
-                    <h3>Titel</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, minus enim quod voluptatum obcaecati nobis molestias earum et fugiat, alias laudantium minima recusandae. Omnis, adipisci. Voluptates sequi nisi magni non dolor nobis maxime cupiditate corporis distinctio, excepturi facilis quibusdam, vero officia quaerat, reprehenderit repellendus deserunt ex iusto eius? Quis laborum explicabo impedit dolorem fuga, neque harum ullam expedita veniam a voluptatibus, nesciunt quam illo facere amet molestiae aliquam! Ducimus rerum ut reiciendis a, voluptatem velit consequuntur? Necessitatibus iusto fugiat culpa nesciunt enim quo, voluptate voluptates quis exercitationem perferendis cum nihil repellendus quisquam nisi quae rerum, dolores esse ut et doloribus.</p>
+                    <h3>Generelt om modeller</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, omnis hic numquam repellat minus sapiente. Nisi est debitis laboriosam libero nobis, iure animi quod ullam quam eos, nulla sunt incidunt ratione possimus, obcaecati eius exercitationem perspiciatis vitae commodi fugit consequatur cupiditate? Voluptates voluptate ut laboriosam beatae nobis iusto est dolor?</p>
                 </div>
                 <div className="model-page-column">
-                    <h3>Titel</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, minus enim quod voluptatum obcaecati nobis molestias earum et fugiat, alias laudantium minima recusandae. Omnis, adipisci. Voluptates sequi nisi magni non dolor nobis maxime cupiditate corporis distinctio, excepturi facilis quibusdam, vero officia quaerat, reprehenderit repellendus deserunt ex iusto eius? Quis laborum explicabo impedit dolorem fuga, neque harum ullam expedita veniam a voluptatibus, nesciunt quam illo facere amet molestiae aliquam! Ducimus rerum ut reiciendis a, voluptatem velit consequuntur? Necessitatibus iusto fugiat culpa nesciunt enim quo, voluptate voluptates quis exercitationem perferendis cum nihil repellendus quisquam nisi quae rerum, dolores esse ut et doloribus.</p>
+                    <h3>ATP Leverancemodel</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur maxime nesciunt debitis aperiam, corporis assumenda dolor laborum. At quos hic id doloremque recusandae delectus, dolor reprehenderit dolorem reiciendis vitae iste repellat earum a sint consectetur officia! Veniam ullam velit cum natus reprehenderit aliquam. Maxime vitae distinctio sequi nisi iure saepe?</p>
+                </div>
+            </div>
+            <div className="model-page-row">
+                <div className="model-page-column">
+                    <h3>Udvikling med ekstern leverandør</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, omnis hic numquam repellat minus sapiente. Nisi est debitis laboriosam libero nobis, iure animi quod ullam quam eos, nulla sunt incidunt ratione possimus, obcaecati eius exercitationem perspiciatis vitae commodi fugit consequatur cupiditate? Voluptates voluptate ut laboriosam beatae nobis iusto est dolor?</p>
+                </div>
+                <div className="model-page-column">
+                    <h3>Koncern Forandringsunivers</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur maxime nesciunt debitis aperiam, corporis assumenda dolor laborum. At quos hic id doloremque recusandae delectus, dolor reprehenderit dolorem reiciendis vitae iste repellat earum a sint consectetur officia! Veniam ullam velit cum natus reprehenderit aliquam. Maxime vitae distinctio sequi nisi iure saepe?</p>
+                </div>
+            </div>
+            <div className="model-page-row">
+                <div className="model-page-column">
+                    <h3>IT Operation Model (ITOM)</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, omnis hic numquam repellat minus sapiente. Nisi est debitis laboriosam libero nobis, iure animi quod ullam quam eos, nulla sunt incidunt ratione possimus, obcaecati eius exercitationem perspiciatis vitae commodi fugit consequatur cupiditate? Voluptates voluptate ut laboriosam beatae nobis iusto est dolor?</p>
                 </div>
             </div>
         </div>
